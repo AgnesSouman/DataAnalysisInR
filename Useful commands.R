0c6c0741b708cc6aea9e8d73d490d71c2b809f5f
Agnes Souman <agnes.souman@outlook.com>
2019-09-29 14:56:33 +0200

    Created initial files.

---
+++ Useful commands.R
@@ -0,0 +1,9 @@
+# This file contains general commands and tips that can be useful in R projects.
+
+# selected line(s) can be run using Ctrl+Enter
+
+# Check the working directory
+getwd()
+
+# Change to the American settings:
+Sys.setlocale("LC_ALL", "C")
